d9d4df49b7da3c8427ee70102db3cd2fa3ac79ea
Ritesh Singh <gettoriteshsingh@gmail.com>
2018-08-08 01:52:52 +0530

    Polls app Made,with less questions.

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-rom django.contrib import admin
+from django.contrib import admin
 
 from .models import Choice, Question
 
